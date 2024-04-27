c06cd8baea36c9168b3acbe40c844cc7528f75d0
yacinebenkaidali <fy_benkaidali@esi.dz>
2024-04-27 19:19:04 +0100

    Added support to run each invocation in a separate goroutine

--- xargs/main.go
+++ xargs/main.go
@@ -19,10 +19,6 @@ type Config struct {
 	errCh chan error
 }
 
-type intervals struct {
-	bounderies [][2]int
-}
-
 func main() {
 	n := flag.Int("n", 5000, "Set the maximum number of arguments taken from standard input for each invocation of utility")
 	p := flag.Int("P", 1, "Parallel mode: run at most maxprocs invocations of utility at once")
@@ -69,57 +65,69 @@ func run(c *Config) error {
 	if err != nil {
 		return err
 	}
-	_ = path
+
 	args := readArgs()
 	allArgs := []string{}
 	allArgs = append(allArgs, cmdArgs...)
 	allArgs = append(allArgs, args...)
 
 	nbInvocations := math.Ceil(float64(len(args)) / float64(c.n))
-	intervals := intervals{
-		bounderies: make([][2]int, int(nbInvocations)),
-	}
+	intervals := make([][2]int, int(nbInvocations))
 	from := 0
 	to := min(from+c.n, len(allArgs))
-	intervals.bounderies[0] = [2]int{from, to}
+	intervals[0] = [2]int{from, to}
 
 	for i := 1; i < int(nbInvocations); i++ {
 		from = to
 		to = min(from+c.n, len(allArgs))
-		intervals.bounderies[i] = [2]int{from, to}
+		intervals[i] = [2]int{from, to}
 	}
 	//spawn `P` goroutines
 	// fmt.Println(intervals.bounderies)
-	processPortion := len(intervals.bounderies)
+	processPortion := len(intervals)
 	from = 0
 	to = 0
 
-	for i := 0; i < c.p; i++ {
-		currentPortion := math.Ceil(float64(processPortion) / float64(c.p-i))
-		from = to
-		to += int(currentPortion)
+	if c.p == 0 {
+		for i := 0; i < len(intervals); i++ {
+			fmt.Println(intervals[i])
+			c.wg.Add(1)
+			go func(i int) {
+				defer c.wg.Done()
+				interval := intervals[i]
+				cmd, err := buildCmd(allArgs, path, interval)
+				if err != nil {
+					c.errCh <- err
+				}
+				if err := cmd.Run(); err != nil {
+					c.errCh <- err
+				}
+			}(i)
+		}
+	} else {
+		for i := 0; i < c.p; i++ {
+			currentPortion := math.Ceil(float64(processPortion) / float64(c.p-i))
+			from = to
+			to += int(currentPortion)
 
-		//launch a goroutine
-		c.wg.Add(1)
-		go func(goid, from, to int) {
-			defer c.wg.Done()
-			for i := 0; i < len(intervals.bounderies[from:to]); i++ {
-				interval := intervals.bounderies[from:to][i]
-				args := allArgs[interval[0]:interval[1]]
-				cmd := exec.Command(path, args...)
-				if len(args) > 0 {
-					cmd.Stdout = os.Stdout
-					cmd.Stderr = os.Stderr
+			//launch a goroutine
+			c.wg.Add(1)
+			go func(goid, from, to int) {
+				defer c.wg.Done()
+				for i := 0; i < len(intervals[from:to]); i++ {
+					interval := intervals[from:to][i]
+					cmd, err := buildCmd(allArgs, path, interval)
+					if err != nil {
+						c.errCh <- err
+					}
 					if err := cmd.Run(); err != nil {
 						c.errCh <- err
 					}
 				}
-				// fmt.Println(cmd.String(), interval, "go", goid)
-			}
-		}(i, from, to)
-		processPortion -= int(math.Ceil(float64(processPortion) / float64(c.p-i)))
+			}(i, from, to)
+			processPortion -= int(math.Ceil(float64(processPortion) / float64(c.p-i)))
+		}
 	}
-
 	return nil
 }
 
@@ -131,4 +139,12 @@ func readArgs() []string {
 	}
 
 	return args
+}
+
+func buildCmd(allArgs []string, path string, interval [2]int) (*exec.Cmd, error) {
+	args := allArgs[interval[0]:interval[1]]
+	cmd := exec.Command(path, args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd, nil
 }
